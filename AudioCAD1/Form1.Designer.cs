e3df012fd5ff65311bf675e1a4a5dd7d455875da
Daniel Feissle <dfeissle@msn.com>
2020-11-05 18:14:48 -0600

    Search bar and rough layout
    Search mechanic added
    Testing audio playback

--- AudioCAD1/Form1.Designer.cs
+++ AudioCAD1/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AudioCAD1
 {
-    partial class Form1
+    partial class frm_main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,248 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mnu_main = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem3 = new System.Windows.Forms.ToolStripSeparator();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.txt_search = new System.Windows.Forms.TextBox();
+            this.lbl_search = new System.Windows.Forms.Label();
+            this.rtx_term_search = new System.Windows.Forms.RichTextBox();
+            this.rtx_stagement = new System.Windows.Forms.RichTextBox();
+            this.lst_select = new System.Windows.Forms.ListBox();
+            this.btn_play = new System.Windows.Forms.Button();
+            this.mnu_main.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnu_main
+            // 
+            this.mnu_main.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.mnu_main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.mnu_main.Location = new System.Drawing.Point(0, 0);
+            this.mnu_main.Name = "mnu_main";
+            this.mnu_main.Size = new System.Drawing.Size(1258, 33);
+            this.mnu_main.TabIndex = 0;
+            this.mnu_main.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.toolStripMenuItem2,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripMenuItem3,
+            this.exportToolStripMenuItem,
+            this.toolStripMenuItem1,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(50, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.newToolStripMenuItem.Text = "New";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(249, 6);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // toolStripMenuItem3
+            // 
+            this.toolStripMenuItem3.Name = "toolStripMenuItem3";
+            this.toolStripMenuItem3.Size = new System.Drawing.Size(249, 6);
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(249, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(202, 30);
+            this.clearToolStripMenuItem.Text = "Audio Library";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.clearToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem1,
+            this.aboutToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(61, 29);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // helpToolStripMenuItem1
+            // 
+            this.helpToolStripMenuItem1.Name = "helpToolStripMenuItem1";
+            this.helpToolStripMenuItem1.Size = new System.Drawing.Size(146, 30);
+            this.helpToolStripMenuItem1.Text = "Help";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(146, 30);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txt_search
+            // 
+            this.txt_search.Location = new System.Drawing.Point(12, 544);
+            this.txt_search.Name = "txt_search";
+            this.txt_search.Size = new System.Drawing.Size(1234, 26);
+            this.txt_search.TabIndex = 1;
+            this.txt_search.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.txt_search.Enter += new System.EventHandler(this.txt_search_Enter);
+            // 
+            // lbl_search
+            // 
+            this.lbl_search.AutoSize = true;
+            this.lbl_search.Location = new System.Drawing.Point(977, 939);
+            this.lbl_search.Name = "lbl_search";
+            this.lbl_search.Size = new System.Drawing.Size(77, 20);
+            this.lbl_search.TabIndex = 2;
+            this.lbl_search.Text = "SEARCH";
+            // 
+            // rtx_term_search
+            // 
+            this.rtx_term_search.Location = new System.Drawing.Point(12, 626);
+            this.rtx_term_search.Name = "rtx_term_search";
+            this.rtx_term_search.Size = new System.Drawing.Size(1234, 306);
+            this.rtx_term_search.TabIndex = 3;
+            this.rtx_term_search.Text = "";
+            // 
+            // rtx_stagement
+            // 
+            this.rtx_stagement.Location = new System.Drawing.Point(12, 576);
+            this.rtx_stagement.Name = "rtx_stagement";
+            this.rtx_stagement.Size = new System.Drawing.Size(1234, 39);
+            this.rtx_stagement.TabIndex = 4;
+            this.rtx_stagement.Text = "";
+            // 
+            // lst_select
+            // 
+            this.lst_select.FormattingEnabled = true;
+            this.lst_select.ItemHeight = 20;
+            this.lst_select.Location = new System.Drawing.Point(12, 626);
+            this.lst_select.Name = "lst_select";
+            this.lst_select.Size = new System.Drawing.Size(1234, 304);
+            this.lst_select.TabIndex = 5;
+            // 
+            // btn_play
+            // 
+            this.btn_play.Location = new System.Drawing.Point(849, 288);
+            this.btn_play.Name = "btn_play";
+            this.btn_play.Size = new System.Drawing.Size(116, 44);
+            this.btn_play.TabIndex = 6;
+            this.btn_play.Text = "Play";
+            this.btn_play.UseVisualStyleBackColor = true;
+            this.btn_play.Click += new System.EventHandler(this.btn_play_Click);
+            // 
+            // frm_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1258, 968);
+            this.Controls.Add(this.btn_play);
+            this.Controls.Add(this.lst_select);
+            this.Controls.Add(this.rtx_stagement);
+            this.Controls.Add(this.rtx_term_search);
+            this.Controls.Add(this.lbl_search);
+            this.Controls.Add(this.txt_search);
+            this.Controls.Add(this.mnu_main);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mnu_main;
+            this.MaximizeBox = false;
+            this.Name = "frm_main";
+            this.Text = "Audio-";
+            this.Load += new System.EventHandler(this.frm_main_Load);
+            this.mnu_main.ResumeLayout(false);
+            this.mnu_main.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mnu_main;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox txt_search;
+        private System.Windows.Forms.Label lbl_search;
+        private System.Windows.Forms.RichTextBox rtx_term_search;
+        private System.Windows.Forms.RichTextBox rtx_stagement;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem3;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ListBox lst_select;
+        private System.Windows.Forms.Button btn_play;
     }
 }
 
--- AudioCAD1/Form1.cs
+++ AudioCAD1/Form1.cs
@@ -5,14 +5,151 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Collections;
+using System.Media;
 
 namespace AudioCAD1
 {
-    public partial class Form1 : Form
+    public partial class frm_main : Form
     {
-        public Form1()
+
+
+
+        string []sf_Name = null;
+
+
+
+
+
+        public frm_main()
         {
             InitializeComponent();
+        }
+
+        private void frm_main_Load(object sender, EventArgs e)
+        {
+           var  allFiles = Directory.GetFiles(Environment.CurrentDirectory, "*.wav", SearchOption.AllDirectories);
+
+            sf_Name = allFiles;
+
+        }
+
+
+
+
+
+
+
+
+
+
+        int priorSpace = 0; //it will always be at 0
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //we want to construct sentences based on spaces
+            if (txt_search.Text!="")
+            {
+              
+                if (txt_search.Text.Substring(txt_search.Text.Length - 1, 1) == " ")
+                {
+                    lbl_search.Text = "SPACE"+priorSpace;
+                    priorSpace = txt_search.Text.Length;
+               //     rtx_term_search.Text = "";
+
+                }
+                else
+                {
+
+
+ 
+
+                    lbl_search.Text = "SEARCH"+priorSpace;
+
+
+                    lst_select.Items.Clear();
+                   // rtx_term_search.Text = "";
+                    for (int i = 0; i < sf_Name.Length; i++)
+                    {
+                        int lastele = sf_Name[i].Split('\\').Length;
+                        if (priorSpace< txt_search.Text.Length) //avoid being at the very end or beginign
+                        {
+                            if (sf_Name[i].Split('\\')[lastele - 1].Contains(txt_search.Text.Substring(priorSpace)))
+                            {
+                             //   rtx_term_search.Text = rtx_term_search.Text + sf_Name[i].Split('\\')[lastele - 1] + Environment.NewLine;
+                                lst_select.Items.Add(sf_Name[i].Split('\\')[lastele - 1]);
+                            }
+                        }
+                        else //backspace detected
+                        {
+                            for (int z=txt_search.Text.Length-1;z>0;z--)
+                            {
+                                //get the space furthest back and reset
+                                string haha = txt_search.Text.Substring(z,1);
+                                if (haha==" ")
+                                {
+                                    priorSpace = z+1;
+                                    i = 0; // research
+                                    break;
+                                }
+                                else if (!txt_search.Text.Contains(" "))
+                                {
+                                    //no spaces exist
+                                    priorSpace =0;
+                                    i = 0; // research
+                                    break;
+                                }
+                                else if (txt_search.Text.Substring(z,1)!="")
+                                {
+                                    //no spaces exist
+                                 //   priorSpace = 0;
+                                 //   i = 0; // research
+                                 //   break;
+                                }
+                            }
+                        }
+
+                    }
+
+
+
+
+
+
+                }
+
+
+                //put in our autoselect for closest matching word
+          
+
+
+            }
+            else
+            {
+                lst_select.Items.Clear();
+            }
+              
+
+        }
+
+        private void txt_search_Enter(object sender, EventArgs e)
+        {
+            rtx_term_search.Text = "";
+        }
+
+        private void clearToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_play_Click(object sender, EventArgs e)
+        {
+            rtx_stagement.Text = lst_select.Items[lst_select.SelectedIndex].ToString();
+            SoundPlayer simpleSound = new SoundPlayer("data\\audio\\MaxxSteele\\" + lst_select.Items[lst_select.SelectedIndex].ToString());
+            simpleSound.Play();
         }
     }
 }
